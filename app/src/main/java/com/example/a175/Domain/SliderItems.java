37b79ce3b2ba22caa593b9938fa385dadec34f8e
DongKey03 <dongpham24703@gmail.com>
2024-06-24 16:28:12 +0700

    update

---
+++ app/src/main/java/com/example/a175/Domain/SliderItems.java
@@ -0,0 +1,22 @@
+package com.example.a175.Domain;
+
+public class SliderItems {
+    private String url;
+
+    public SliderItems() {
+    }
+
+    public SliderItems(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+}
