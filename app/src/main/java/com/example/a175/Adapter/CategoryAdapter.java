b68eb60b66c5b0810382e30be88129fa62fb76cd
DongKey03 <dongpham24703@gmail.com>
2024-06-24 16:26:23 +0700

    update

---
+++ app/src/main/java/com/example/a175/Adapter/CategoryAdapter.java
@@ -0,0 +1,54 @@
+package com.example.a175.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.a175.Domain.CategoryDomain;
+import com.example.a175.databinding.ViewholderCategoryBinding;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private ArrayList<CategoryDomain> items;
+
+    public CategoryAdapter(ArrayList<CategoryDomain> items) {
+        this.items = items;
+    }
+
+    private Context context;
+    @NonNull
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        ViewholderCategoryBinding binding = ViewholderCategoryBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
+        holder.binding.title.setText(items.get(position).getTitle());
+
+        Glide.with(context)
+                .load(items.get(position).getPicUrl())
+                .into(holder.binding.pic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ViewholderCategoryBinding binding;
+
+        public ViewHolder(ViewholderCategoryBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
