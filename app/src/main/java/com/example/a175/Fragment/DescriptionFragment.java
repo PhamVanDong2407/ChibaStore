8a9734b3571c71670b5af540c182920549ba870d
DongKey03 <dongpham24703@gmail.com>
2024-06-24 16:28:20 +0700

    update

---
+++ app/src/main/java/com/example/a175/Fragment/DescriptionFragment.java
@@ -0,0 +1,36 @@
+package com.example.a175.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.a175.R;
+
+public class DescriptionFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView descTxt = view.findViewById(R.id.descriptionTxt);
+        descTxt.setText(getArguments().getString("description", ""));
+    }
+}
